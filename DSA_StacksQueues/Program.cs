969d983dfff7ba570fd0da433a7e2063c687496c
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-19 16:16:40 +0530

    [SUNIL]:Create A Simple Queue

--- DSA_StacksQueues/Program.cs
+++ DSA_StacksQueues/Program.cs
@@ -5,20 +5,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To StackAndQueue Operation!\n");
-            Stack obj = new Stack();
-            obj.Push(70);
-            obj.Push(30);
-            obj.Push(56);        
-            obj.display();
-            obj.Peak();
-            obj.Pop();
-            obj.display();
-            obj.Peak();
-            obj.Pop();
-            obj.display();
-            obj.Peak();
-            obj.Pop();
-            obj.display();
+            //Stack obj = new Stack();
+            //obj.Push(70);
+            //obj.Push(30);
+            //obj.Push(56);        
+            //obj.display();
+            //obj.Peak();
+            //obj.Pop();
+            //obj.display();
+            //obj.Peak();
+            //obj.Pop();
+            //obj.display();
+            //obj.Peak();
+            //obj.Pop();
+            //obj.display();
+            
+            Queue obj = new Queue();
+            obj.Enqueue(56);
+            obj.Enqueue(30);
+            obj.Enqueue(70);
+            Console.WriteLine("\nThe Elements Are Present In The Queue");
+            obj.Display();
         }
     }
 }
---
+++ DSA_StacksQueues/Queue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DSA_StacksQueues
+{
+    internal class Queue
+    {
+        Node first;
+        Node last;
+        public Queue()
+        {
+            this.first = this.last = null;
+        }
+        internal void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.last == null)
+            {
+                this.first = this.last = newNode;
+            }
+            else
+            {
+                this.last.next = newNode;
+                this.last = newNode;
+            }
+            Console.WriteLine("{0} is inserted into Queue", data);
+        }
+        public void Display()
+        {
+            Node temp = this.first;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.id + "\n");
+                temp = temp.next;
+            }
+        }
+    }
+}
